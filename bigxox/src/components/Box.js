d6b610db8055b1c1d2132c25c1f4dd5f4a6046dd
Taha <tahapasha121@gmail.com>
2024-03-27 02:38:47 +0530

    third commit for today

--- bigxox/src/components/Box.js
+++ bigxox/src/components/Box.js
@@ -2,18 +2,27 @@ import { useState } from "react"
 import { useDispatch } from "react-redux";
 import { nexturn } from "../utilities/sliceofstore/cartSlice";
 import { useSelector } from "react-redux";
-export const Box=()=>{
+export const Box=(props)=>{
+    const [color,setcolor]=useState("w-20 h-20 rounded-2xl shadow-lg border cursor-pointer flex justify-center items-center text-4xl font-bold text-yellow-400");
     const flagger=useSelector((store)=>store.cart.flag);
     console.log(flagger);
    
     const[valueofbox,setvalueofbox]=useState("");
     const dispatch=useDispatch();
     return(
-        <div className="w-20 h-20 rounded-2xl shadow-lg border cursor-pointer flex justify-center
-        items-center text-4xl font-bold text-purple-700"  onClick={()=>{
+        <div className={color}  onClick={()=>{
            {
-            setvalueofbox(flagger===true?"X":"O");
-            
+            if(flagger){
+                setcolor("w-20 h-20 rounded-2xl shadow-lg border cursor-pointer flex justify-center items-center text-4xl font-bold text-yellow-400")
+            setvalueofbox("X");
+            props.passthis();
+        }
+            else{
+                
+                setcolor("w-20 h-20 rounded-2xl shadow-lg border cursor-pointer flex justify-center items-center text-4xl font-bold text-purple-700")
+                setvalueofbox("O");
+                props.pathisfoy();
+            }
             dispatch(nexturn());}
         }}>{valueofbox}</div>
     )
--- bigxox/src/components/Smallxox.js
+++ bigxox/src/components/Smallxox.js
@@ -1,12 +1,57 @@
- import { Box } from "./Box"
+ import { useState } from "react"
+import { Box } from "./Box"
+ 
  export const Smallxox=()=>{
+
+    const[arrayof1,setarrayof1]=useState([0,0,0,0,0,0,0,0,0]);
+    const[arrayof2,setarrayof2]=useState([0,0,0,0,0,0,0,0,0]);
+    function check(arr){
+        console.log("i have been claaed");
+        const newarray=[...arr];
+        const temp=[[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
+        for(let i in temp)
+        {
+            console.log();
+            if(newarray[temp[i][0]]+newarray[temp[i][1]]+newarray[temp[i][2]]===3)
+            {
+                return true;
+            }
+
+        }
+        return false;
+    }
+    
+    const putxforarray1=(index)=>{
+        const newarray=[...arrayof1];
+        newarray[index]=1;
+        setarrayof1(newarray);
+    }
+    const putoforarray2=(index)=>{
+        const newarray=[...arrayof2];
+        newarray[index]=1;
+        setarrayof2(newarray);
+    }
+    console.log(arrayof1);
+    console.log(arrayof2);
     const arr=[0,1,2]
+    if(check(arrayof1))
+    {
+        return(<div className="w-[288px] h-[288px] font-bold text-green-600 text-9xl flex justify-center items-center">X</div>)
+    }
+    if(check(arrayof2))
+    {
+        return(<div className="w-[288px] h-[288px] font-bold text-red-600 text-9xl flex justify-center items-center">O</div>)
+    }
     return(
+        
+        
         <div className="flex-col gap-y-7 p-4">
             <div className=" flex gap-2 ">
                {
                 arr.map((item,i)=>{
-                    return(<Box key={i} />)
+                    return(<Box key={i} passthis={()=>{putxforarray1(i)}}  pathisfoy={()=>{
+                        putoforarray2(i)
+                    }}/>)
                 })
                 
                }
@@ -16,7 +61,9 @@
             <div className="flex gap-2 mt-2">
             {
                 arr.map((item,i)=>{
-                    return(<Box key={i+3}/>)
+                    return(<Box key={i+3} passthis={()=>{putxforarray1(i+3)}} pathisfoy={()=>{
+                        putoforarray2(i+3)
+                    }}/>)
                 })
                 
                }
@@ -25,7 +72,9 @@
             <div className="flex gap-2 cursor-pointer mt-2">
             {
                 arr.map((item,i)=>{
-                    return(<Box key={i+6}/>)
+                    return(<Box key={i+6} passthis={()=>{putxforarray1(i+6)}} pathisfoy={()=>{
+                        putoforarray2(i+6)
+                    }}/>)
                 })
                 
                }
