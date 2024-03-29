4182e44eae5617201eb3d84b074de5c96e8c5827
Taha <tahapasha121@gmail.com>
2024-03-29 19:45:09 +0530

    last commit of the day

--- src/componenets/Inception.js
+++ src/componenets/Inception.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import { Smallxox } from "./Smallxox"
+import { useSelector } from "react-redux";
 
 export const Inception=()=>{
+    const flag=useSelector(store=>store.cart.flag);
     const arr=[0,0,0,0,0,0,0,0,0];
+    const borderyellow="w-15 h-18  p-6 border-4 border-yellow-300 rounded-xl";
+    const textfont="text-6xl font-bold  ";
+
     
 
     
     return(
         
-        
-
-        <div className="grid grid-cols-3 w-[700px] gap-x-4 gap-y-4 ">
+        <div >
+            <div className=" text-center text-3xl">Let's play</div>
+            <div className="flex justify-center items-center">
+          <div className={textfont+(flag&&borderyellow)}>X</div>
+        <div className="grid grid-cols-3 w-[700px] gap-x-4 gap-y-4 scale-[0.8]">
             {
                 arr.map((obj,i)=>{
                    
@@ -19,6 +26,9 @@ export const Inception=()=>{
                 })
             }
 
+        </div>
+        <div className={textfont+(!flag&&borderyellow)}>O</div>
+        </div>
         </div>
         
     )
