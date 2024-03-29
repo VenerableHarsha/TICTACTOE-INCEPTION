b875fade8c1c2f12c5d0aa5556080f5d134b1990
Taha <tahapasha121@gmail.com>
2024-03-29 18:39:06 +0530

    commit 3 of the day adding backgrounds to winner

--- src/componenets/Smallxox.js
+++ src/componenets/Smallxox.js
@@ -9,8 +9,9 @@ export const Smallxox = (props) => {
     const matrix = useSelector(store => store.cart.inceptionmat);//subscribing to 9x9 matrix
     const flagof = useSelector(state => state.cart.who_won_flag);//subscribing to who_one_flag
     /// so if lets say is wins the first smallbox componenet who_won flag will become=['x',"","","","","",,,,,,,,,,,,,,,] and vise versa
-    let border=" flex justify-center  py-2";
+    let border=" flex justify-center  py-2 ";
     const subs_index_of_xox=useSelector(store=>store.cart.index_of_small_xox);
+    const [winnercolor,setwinnercolor]=useState("");
 
     useEffect(() => {
         function check(arr1) {//function to check if player has won the smallbox componenet
@@ -20,11 +21,12 @@ export const Smallxox = (props) => {
                 for (let i in temp) {
                     if (newarray[temp[i][0]] + newarray[temp[i][1]] + newarray[temp[i][2]] === 3) {
                         dispatch(mark_x_or_O_in_flag([props.indexofsmall, "X"]));
+                       setwinnercolor("bg-green-400 rounded-xl");
                         setPlayer({
                             winner:"X",
                             iswinner:true
                         });
-                        console.log("X is winner");
+                        console.log("X is winner ");
                         break;
                     }
                     if (newarray[temp[i][0]] + newarray[temp[i][1]] + newarray[temp[i][2]] === 18) {
@@ -33,6 +35,7 @@ export const Smallxox = (props) => {
                             winner:"O",
                             iswinner:true
                         });
+                        setwinnercolor("bg-red-400 rounded-xl")
                         console.log("O is winner");
                         break;
                     }
@@ -43,16 +46,16 @@ export const Smallxox = (props) => {
     }, [matrix, flagof, dispatch, props.indexofsmall]);
     if(props.indexofsmall===subs_index_of_xox)
     {
-        border=" flex justify-center  py-2 border-4 rounded-xl"
+        border=" flex justify-center  py-2 border-4 rounded-xl border-green-600 "
     }
     else{
-        border=" flex justify-center  py-2";
+        border=" flex justify-center  py-2 ";
     }
     
 
     return (
-        <div className={border}>
-            {player.iswinner&&<label> {player.winner} won</label>}
+        <div className={border+winnercolor}>
+            {false&&<label className="absolute text-3xl"> {player.winner}</label>}
             <div className="grid grid-cols-3 w-44">
                 {
                     Array(9).fill().map((_, i) => (
